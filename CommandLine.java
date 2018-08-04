cb39f975f388ef8b21f4d15da02ff9b675adda0f
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 23:45:50 +0530

    Update CommandLine.java

--- CommandLine.java
+++ CommandLine.java
@@ -1,10 +1,9 @@
 
 public class CommandLine {
 	
-
 	public static void main(String args[]) {
 		int n = 0;
-		for(int i=0;i<3;i++) {
+		for(int i=0;i<3;i++) {					//Loop for taking integer from terminal
 			n = n + Integer.parseInt(args[i]);
 		}
 		System.out.println("The sum of three number is "+n);
